7d8cb8b0484271e26443303540246124ab5b6aad
danielabouelezz <75901821+danielabouelezz@users.noreply.github.com>
2024-09-09 21:59:41 +0300

    adding and example about the usage of booalpha and flush

--- On Output and Input/SetiosFlags.cpp
+++ On Output and Input/SetiosFlags.cpp
@@ -9,6 +9,6 @@ int main()
     cout<<"|" <<setw(20) <<setiosflags(ios::left)<<142.1<<"|\n";// |               142.1|
     cout<<"|" <<setw(20) <<setiosflags(ios::scientific)<<142.1<<"|\n";// |142.1               |
     cout<<"|" <<setw(20) <<setiosflags(ios::fixed)<<142.1<<"|\n";// |1.421000e+002       |
-    cout<<"|" <<setw(20) <<setiosflags(ios::right)<<142.1<<"|\n";// |142.1               |
-    return 0;
+    cout<<"|" <<setw(20) <<setiosflags(ios::right)<<142.1<<"|\n";// |               142.1|
+    return  0;
 }
---
+++ On Output and Input/others.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip> 
+
+using namespace std;
+
+int main()
+{
+    bool status = true;
+    cout<<status<< "\n";
+    cout<<boolalpha<<status<<"\n";
+    cout<<"some input" <<flush<<"\n";
+
+    return 0;
+}
