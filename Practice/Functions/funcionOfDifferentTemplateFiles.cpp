829ef27d58360aa17a220e741069168a1117fb13
danielabouelezz <75901821+danielabouelezz@users.noreply.github.com>
2024-09-12 20:34:19 +0300

    editing the functionOfDifferentTemplateFiles.cpp

--- Practice/Functions/funcionOfDifferentTemplateFiles.cpp
+++ Practice/Functions/funcionOfDifferentTemplateFiles.cpp
@@ -13,6 +13,8 @@ int main()
     std::cout<< maximum(4, 6) <<"\n";//6
     std::cout<< maximum(2.3, 5.6)<<"\n";//5.6
     std::cout<< maximum(2, 3.5) <<"\n";//3
+    std::cout<< maximum<int>(3.5, 5)<<"\n";//5
+    std::cout<< maximum<double, int>(3.5, 5)<<"\n";//5.0
 
     return 0;
 }
