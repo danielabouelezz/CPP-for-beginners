1064e9456654b21e95c49da91cf6226ddcc0c3b0
danielabouelezz <75901821+danielabouelezz@users.noreply.github.com>
2024-11-21 15:23:16 +0200

    offtopic, pointer to function example

--- Structures/structures1.cpp
+++ Structures/structures1.cpp
@@ -83,46 +83,81 @@
 
 // Fith example: sorting struct array based on specific criterea
 
-struct Employee{
-    std::string name; 
-    int salary; 
-    int position; 
-};
+// struct Employee{
+//     std::string name; 
+//     int salary; 
+//     int position; 
+// };
 
-// Writing wrong function may cause RTE
-// When return true, means first object must come before second object
-// if we called cmp(A, B) and returned true, NEBER have cmp(B, A) = true
-// If A == B, then both cmp(A, B) and cmp(B, A) MUST give false
+// // Writing wrong function may cause RTE
+// // When return true, means first object must come before second object
+// // if we called cmp(A, B) and returned true, NEBER have cmp(B, A) = true
+// // If A == B, then both cmp(A, B) and cmp(B, A) MUST give false
 
-bool cmp(const Employee & first, const Employee &second)    // compare function
-{   // The right comparison should be field by field based on YOUR priority
+// bool cmp(const Employee & first, const Employee &second)    // compare function
+// {   // The right comparison should be field by field based on YOUR priority
 
-    // if first field NOT equal, then it determines the answer
-    if(first.salary != second.salary)
-        // Let bigger salary be preferred
-        return first.salary > second.salary; 
+//     // if first field NOT equal, then it determines the answer
+//     if(first.salary != second.salary)
+//         // Let bigger salary be preferred
+//         return first.salary > second.salary; 
 
-    // Now same salary, name to name
-    if(first.name != second.name)
-        return first.name < second.name; 
+//     // Now same salary, name to name
+//     if(first.name != second.name)
+//         return first.name < second.name; 
     
-    // Now, the remaining is the position. DIRECTLY compare here. No if codition
-    return first.position < second.position; 
+//     // Now, the remaining is the position. DIRECTLY compare here. No if codition
+//     return first.position < second.position; 
+// }
+
+// int main()
+// {
+//     Employee emps[5] = {
+//         {"baher", 70, 5}, {"zyad", 99, 10}, {"ali", 70, 2}, 
+//         {"baher", 70, 5}, {"baher", 70, 3} 
+//     };
+
+//     // cmp function: Let sort based on, bigger salary, 
+//         // if tie smaller name, if tie smaller position
+//     std::sort(emps, emps + 5, cmp); 
+
+//     for(int i{0}; i < 5; ++i){
+//         Employee & emp = emps[i];
+//         std::cout << emp.salary <<" " << emp.name << " " << emp.position <<"\n"; 
+//     }
+// }
+
+
+// Offtopic : Pointer to function
+
+int sum(int n, int m)
+{
+    return n + m; 
+}
+
+int mul (int n, int m)
+{
+    return n * m; 
+}
+
+// pointer to function that takes two integers
+int (*functionPtr)(int, int); 
+
+int compute(int a, int b, int (*functionPtr)(int, int))
+{
+    return (*functionPtr)(a, b); 
 }
 
 int main()
 {
-    Employee emps[5] = {
-        {"baher", 70, 5}, {"zyad", 99, 10}, {"ali", 70, 2}, 
-        {"baher", 70, 5}, {"baher", 70, 3} 
-    };
+    functionPtr = &sum; // address of function
+    int val = (*functionPtr)(2, 5); 
 
-    // cmp function: Let sort based on, bigger salary, 
-        // if tie smaller name, if tie smaller position
-    std::sort(emps, emps + 5, cmp); 
+    val = compute(2, 5, mul); 
 
-    for(int i{0}; i < 5; ++i){
-        Employee & emp = emps[i];
-        std::cout << emp.salary <<" " << emp.name << " " << emp.position <<"\n"; 
-    }
+    // array of pointers to function
+    int (*p[2]) (int x, int y); 
+    p[0] = sum; 
+    p[1] = mul; 
+    val = (*p[0])(3, 2); 
 }
