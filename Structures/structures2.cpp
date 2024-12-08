bc84934ffe9263ce47ee74be1508735bc1e116d6
danielabouelezz <75901821+danielabouelezz@users.noreply.github.com>
2024-12-08 16:19:33 +0200

    example of methods in structure

---
+++ Structures/structures2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>  
+
+struct Employee{
+    std::string name; 
+    int salary; 
+
+    void SetSalary(int value)   // Method NOT Function
+    {
+        salary = value; 
+    }
+
+    void AddSalary(int value); 
+};
+
+void Employee::AddSalary(int value) // We can define method outside too
+{
+    salary += value; 
+}
+
+int main()
+{
+    Employee emp; 
+    std::cout<<emp.salary <<"\n";   //garbage, you may be lucky and get zero
+
+    emp.SetSalary(100); 
+    std::cout<<emp.salary<<"\n";    //100
+
+    emp.AddSalary(25); 
+    std::cout<<emp.salary<<"\n"; 
+}
